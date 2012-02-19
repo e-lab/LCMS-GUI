8f15526f7a121f51c6bc6b21d7c0b59b197c37b5
Berin Martini <berin.martini@gmail.com>
2012-02-19 02:13:59 -0500

    Whitespace changes for SimpleQueue

--- SourceCode/SimpleQueue.h
+++ SourceCode/SimpleQueue.h
@@ -28,12 +28,11 @@
 #include <queue>
 #include "wx/afterstd.h"
 
-enum wxMessageQueueError
-{
-    wxMSGQUEUE_NO_ERROR = 0, // operation completed successfully
-    wxMSGQUEUE_TIMEOUT,      // no messages received before timeout expired
-    wxMSGQUEUE_MISC_ERROR,   // some unexpected (and fatal) error has occurred
-    wxMSGQUEUE_EMTPY         // queue was empty
+enum wxMessageQueueError {
+	wxMSGQUEUE_NO_ERROR = 0, // operation completed successfully
+	wxMSGQUEUE_TIMEOUT,      // no messages received before timeout expired
+	wxMSGQUEUE_MISC_ERROR,   // some unexpected (and fatal) error has occurred
+	wxMSGQUEUE_EMTPY         // queue was empty
 };
 
 // ---------------------------------------------------------------------------
@@ -51,141 +50,131 @@ template <typename T>
 class SimpleQueue
 {
 public:
-    // The type of the messages transported by this queue
-    typedef T Message;
+	// The type of the messages transported by this queue
+	typedef T Message;
 
-    // Default ctor creates an initially empty queue
-    SimpleQueue()
-       : m_conditionNotEmpty(m_mutex)
-    {
-    }
+	// Default ctor creates an initially empty queue
+	SimpleQueue()
+			: m_conditionNotEmpty (m_mutex) {
+	}
 
-    // Add a message to this queue and signal the threads waiting for messages.
-    //
-    // This method is safe to call from multiple threads in parallel.
-    wxMessageQueueError Post(const Message& msg)
-    {
-        wxMutexLocker locker(m_mutex);
+	// Add a message to this queue and signal the threads waiting for messages.
+	//
+	// This method is safe to call from multiple threads in parallel.
+	wxMessageQueueError Post (const Message& msg) {
+		wxMutexLocker locker (m_mutex);
 
-        wxCHECK( locker.IsOk(), wxMSGQUEUE_MISC_ERROR );
+		wxCHECK (locker.IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        m_messages.push(msg);
+		m_messages.push (msg);
 
-        m_conditionNotEmpty.Signal();
+		m_conditionNotEmpty.Signal();
 
-        return wxMSGQUEUE_NO_ERROR;
-    }
+		return wxMSGQUEUE_NO_ERROR;
+	}
 
-    // Remove all messages from the queue.
-    //
-    // This method is meant to be called from the same thread(s) that call
-    // Post() to discard any still pending requests if they became unnecessary.
-    wxMessageQueueError Clear()
-    {
-        wxCHECK( IsOk(), wxMSGQUEUE_MISC_ERROR );
+	// Remove all messages from the queue.
+	//
+	// This method is meant to be called from the same thread(s) that call
+	// Post() to discard any still pending requests if they became unnecessary.
+	wxMessageQueueError Clear() {
+		wxCHECK (IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        wxMutexLocker locker(m_mutex);
+		wxMutexLocker locker (m_mutex);
 
-        std::queue<T> empty;
-        std::swap(m_messages, empty);
+		std::queue<T> empty;
+		std::swap (m_messages, empty);
 
-        return wxMSGQUEUE_NO_ERROR;
-    }
+		return wxMSGQUEUE_NO_ERROR;
+	}
 
-    // Wait no more than timeout milliseconds until a message becomes available.
-    //
-    // Setting timeout to 0 is equivalent to an infinite timeout. See Receive().
-    wxMessageQueueError ReceiveTimeout(long timeout, T& msg)
-    {
-        wxCHECK( IsOk(), wxMSGQUEUE_MISC_ERROR );
+	// Wait no more than timeout milliseconds until a message becomes available.
+	//
+	// Setting timeout to 0 is equivalent to an infinite timeout. See Receive().
+	wxMessageQueueError ReceiveTimeout (long timeout, T& msg) {
+		wxCHECK (IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        wxMutexLocker locker(m_mutex);
+		wxMutexLocker locker (m_mutex);
 
-        wxCHECK( locker.IsOk(), wxMSGQUEUE_MISC_ERROR );
+		wxCHECK (locker.IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        const wxMilliClock_t waitUntil = wxGetLocalTimeMillis() + timeout;
-        while ( m_messages.empty() )
-        {
-            wxCondError result = m_conditionNotEmpty.WaitTimeout(timeout);
+		const wxMilliClock_t waitUntil = wxGetLocalTimeMillis() + timeout;
+		while (m_messages.empty()) {
+			wxCondError result = m_conditionNotEmpty.WaitTimeout (timeout);
 
-            if ( result == wxCOND_NO_ERROR )
-                continue;
+			if (result == wxCOND_NO_ERROR)
+				continue;
 
-            wxCHECK( result == wxCOND_TIMEOUT, wxMSGQUEUE_MISC_ERROR );
+			wxCHECK (result == wxCOND_TIMEOUT, wxMSGQUEUE_MISC_ERROR);
 
-            const wxMilliClock_t now = wxGetLocalTimeMillis();
+			const wxMilliClock_t now = wxGetLocalTimeMillis();
 
-            if ( now >= waitUntil )
-                return wxMSGQUEUE_TIMEOUT;
+			if (now >= waitUntil)
+				return wxMSGQUEUE_TIMEOUT;
 
-            timeout = (waitUntil - now).ToLong();
-            wxASSERT(timeout > 0);
-        }
+			timeout = (waitUntil - now).ToLong();
+			wxASSERT (timeout > 0);
+		}
 
-        msg = m_messages.front();
-        m_messages.pop();
+		msg = m_messages.front();
+		m_messages.pop();
 
-        return wxMSGQUEUE_NO_ERROR;
-    }
+		return wxMSGQUEUE_NO_ERROR;
+	}
 
-    // Same as ReceiveTimeout() but waits for as long as it takes for a message
-    // to become available (so it can't return wxMSGQUEUE_TIMEOUT)
-    wxMessageQueueError Receive(T& msg)
-    {
-        wxCHECK( IsOk(), wxMSGQUEUE_MISC_ERROR );
+	// Same as ReceiveTimeout() but waits for as long as it takes for a message
+	// to become available (so it can't return wxMSGQUEUE_TIMEOUT)
+	wxMessageQueueError Receive (T& msg) {
+		wxCHECK (IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        wxMutexLocker locker(m_mutex);
+		wxMutexLocker locker (m_mutex);
 
-        wxCHECK( locker.IsOk(), wxMSGQUEUE_MISC_ERROR );
+		wxCHECK (locker.IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        while ( m_messages.empty() )
-        {
-            wxCondError result = m_conditionNotEmpty.Wait();
+		while (m_messages.empty()) {
+			wxCondError result = m_conditionNotEmpty.Wait();
 
-            wxCHECK( result == wxCOND_NO_ERROR, wxMSGQUEUE_MISC_ERROR );
-        }
+			wxCHECK (result == wxCOND_NO_ERROR, wxMSGQUEUE_MISC_ERROR);
+		}
 
-        msg = m_messages.front();
-        m_messages.pop();
+		msg = m_messages.front();
+		m_messages.pop();
 
-        return wxMSGQUEUE_NO_ERROR;
-    }
+		return wxMSGQUEUE_NO_ERROR;
+	}
 
-    // Same as Receive() but will not wait if the queue is empty.
-    wxMessageQueueError ReceiveNoWait(T& msg)
-    {
-        wxCHECK( IsOk(), wxMSGQUEUE_MISC_ERROR );
+	// Same as Receive() but will not wait if the queue is empty.
+	wxMessageQueueError ReceiveNoWait (T& msg) {
+		wxCHECK (IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        wxMutexLocker locker(m_mutex);
+		wxMutexLocker locker (m_mutex);
 
-        wxCHECK( locker.IsOk(), wxMSGQUEUE_MISC_ERROR );
+		wxCHECK (locker.IsOk(), wxMSGQUEUE_MISC_ERROR);
 
-        if ( m_messages.empty() )
-        {
-            return wxMSGQUEUE_EMTPY;
-        }
+		if (m_messages.empty()) {
+			return wxMSGQUEUE_EMTPY;
+		}
 
-        msg = m_messages.front();
-        m_messages.pop();
+		msg = m_messages.front();
+		m_messages.pop();
 
-        return wxMSGQUEUE_NO_ERROR;
-    }
+		return wxMSGQUEUE_NO_ERROR;
+	}
 
-    // Return false only if there was a fatal error in ctor
-    bool IsOk() const
-    {
-        return m_conditionNotEmpty.IsOk();
-    }
+	// Return false only if there was a fatal error in ctor
+	bool IsOk() const {
+		return m_conditionNotEmpty.IsOk();
+	}
 
 private:
-    // Disable copy ctor and assignment operator
-    SimpleQueue(const SimpleQueue<T>& rhs);
-    SimpleQueue<T>& operator=(const SimpleQueue<T>& rhs);
+	// Disable copy ctor and assignment operator
+	SimpleQueue (const SimpleQueue<T>& rhs);
+	SimpleQueue<T>& operator= (const SimpleQueue<T>& rhs);
 
-    mutable wxMutex m_mutex;
-    wxCondition     m_conditionNotEmpty;
+	mutable wxMutex m_mutex;
+	wxCondition     m_conditionNotEmpty;
 
-    std::queue<T>   m_messages;
+	std::queue<T>   m_messages;
 };
 
 #endif // wxUSE_THREADS
