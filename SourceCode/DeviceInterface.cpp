74c677ffbba457765fb4d4ac82f0e055d48ea340
Berin Martini <berin.martini@gmail.com>
2012-02-23 22:21:32 -0500

    Add windows memmory debugging code

--- SourceCode/DeviceInterface.cpp
+++ SourceCode/DeviceInterface.cpp
@@ -5,6 +5,17 @@
 #include "DeviceInterface.h"
 #include "DeviceEvent.h"
 
+// When debugging changes all calls to "new" to be calls to "DEBUG_NEW" allowing for memory leaks to
+// give you the file name and line number where it occurred.
+#ifdef _DEBUG
+    #include <crtdbg.h>
+    #define DEBUG_NEW new(_NORMAL_BLOCK ,__FILE__, __LINE__)
+    #define new DEBUG_NEW
+#else
+    #define DEBUG_NEW new
+#endif
+
+
 DeviceInterface::DeviceInterface (wxEvtHandler* displayTmp) : wxThread (wxTHREAD_JOINABLE)
 {
 	pollDevice = false;
